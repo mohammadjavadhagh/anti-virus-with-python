199b3c9616226dfb5dd8dbfe5221c8e174a480b0
mohammadjavadhagh <65391859+mohammadjavadhagh@users.noreply.github.com>
2020-05-15 12:49:07 +0430

    Create register.py

---
+++ register.py
@@ -0,0 +1,25 @@
+import os
+os.system('cls'or'clear')
+
+print("name")
+name = input()
+print("email")
+email = input()
+if "@" in email:
+
+    print("your email configured ")
+
+else:
+    print("your email cant  configured ")
+os.system('exit')
+
+print("password")
+
+password = input()
+
+if "a" in password:
+    print(" your password configured")
+    
+else:
+    print("please a in your password ")
+
