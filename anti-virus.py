1aab48a49b53a1913ecb39db3261b66485269efa
mohammadjavadhagh <65391859+mohammadjavadhagh@users.noreply.github.com>
2020-05-15 13:43:32 +0430

    Update anti-virus.py

--- anti-virus.py
+++ anti-virus.py
@@ -1,1 +1,47 @@
 
+from colorama import Fore, Back, Style
+import time,os
+
+os.system('cls' or 'clear')
+
+
+print(Fore.RED + "anti virus")
+time.sleep(0.1)
+print(Fore.WHITE + "PROGRAMMERS MOHAMMADJAVAD HAGHBEIGI,MAHDI SALEHI KHAH")
+time.sleep(0.1)
+print(Fore.YELLOW"options")
+time.sleep(0.1)
+print(Fore.BLUE + "1 check for virus")
+time.sleep(0.1)
+print(Fore.BLUE + "2 check for rat")
+time.sleep(0.1)
+print(Fore.BLUE + "3 check not hacked")
+time.sleep(0.1)
+print(Fore.BLUE + "4 check the your site of virus")
+time.sleep(0.1)
+print(Fore.BLUE + "5 check the your browser of hacking")
+time.sleep(0.1)
+youroption = input (Fore.YELLO"your option:")
+
+
+if "1" in youroption:
+    print("checking...")
+
+
+if "2" in youroption:
+    print("checking rats...")
+
+if "3" in youroption:
+    print("check four you hacked")
+
+if "4" in youroption:
+    address = input("site addres")
+    print("1 check four the robots")
+    print("2 check four ddos or dos")
+
+if address == '1':
+    print("1 check four the robots...")
+if address == '2':
+    print("2 check four ddos or dos...")
+
+p = input()
