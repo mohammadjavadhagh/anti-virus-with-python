2fc6d93be1cafd160066e8e73925fd85e5c1eedf
mohammadjavadhagh <65391859+mohammadjavadhagh@users.noreply.github.com>
2020-05-15 12:46:15 +0430

    Create anti-virus.py

---
+++ anti-virus.py
@@ -0,0 +1,42 @@
+
+from colorama import Fore, Back, Style
+import time,os
+
+os.system('cls' or 'clear')
+
+
+print(Fore.RED + "anti virus")
+time.sleep(0.1)
+print("PROGRAMMERS MOHAMMADJAVAD HAGHBEIGI,MAHDI SALEHI KHAH")
+time.sleep(0.1)
+print("options")
+time.sleep(0.1)
+print("1 check for virus")
+time.sleep(0.1)
+print("2 check for rat")
+time.sleep(0.1)
+print("3 check not hacked")
+time.sleep(0.1)
+print("4 check the your site of virus")
+time.sleep(0.1)
+print("5 check the your browser of hacking")
+time.sleep(0.1)
+youroption = input ("your option:")
+
+
+if "1" in youroption:
+    print("checking...")
+
+
+if "2" in youroption:
+    print("checking rats...")
+
+if "3" in youroption:
+    print("check four you hacked")
+
+if "4" in youroption:
+    address = input("site addres")
+    print("1 check four the robots")
+    print("2 check four ddos or dos")
+    
+p = input()
